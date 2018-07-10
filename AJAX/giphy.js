57e08429a964596eed20c68521b4ba75dc25743e
mistmr7 <michael.mistarz@gmail.com>
2018-07-10 15:23:27 -0400

    finished AJAX call and added some css styling

--- AJAX/giphy.js
+++ AJAX/giphy.js
@@ -1,10 +1,43 @@
 $(document).ready(function(){
     let topics = ['alligator', 'crocodile', 'dinosaur', 'cow', 'elephant', 'shark', 'kangaroo', 'wallaby', 'python', 'cobra', 'sloth']
 
-    function alertAnimalName(event) {
+    function displayAnimalGifs(event) {
+        $('#animals').empty()
         console.log(event.target)
         let animalName = $(this).attr('data-name')
-        $('#animals').text(animalName)
+        let apiKey = 'd4d526Bu9TK7tTQfpXVTMMJTYUfuh7jG'
+        let queryURL = `https://api.giphy.com/v1/gifs/search?q=${animalName}&limit=10&api_key=${apiKey}`
+        
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(response){
+            console.log(response)
+            for (i=0; i < 10; i++){
+
+            
+            let rating = response.data[i].rating
+            let cardDiv = $('<div>').addClass('card')
+            let ratingDiv = $('<div>').addClass('card-body')
+            ratingDiv.text(`Rating: ${rating.toUpperCase()}`)
+            cardDiv.addClass('col-lg-4')
+            cardDiv.addClass('text-center')
+            $('#animals').append(cardDiv)
+            cardDiv.append(ratingDiv)
+
+            let gifURL = response.data[i].images.fixed_height.url
+            let gifOne = $("<img>").attr('src', gifURL).attr('alt', 'gif')
+            
+            gifOne.addClass('text-center')
+            cardDiv.append(gifOne)
+
+            
+
+            }
+
+        })
+    
+        // $('#animals').text(animalName)
     }
 
     function renderButtons() {
@@ -31,7 +64,7 @@ $(document).ready(function(){
         renderButtons()
     })
 
-    $(document).on('click','.movie-btn', alertAnimalName)
+    $(document).on('click','.movie-btn', displayAnimalGifs)
     
     renderButtons()
 
